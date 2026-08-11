c6711bca637d87a706391469675a6ce7d4f19040
SergeyKacheruk <sergeykacheruk.job@gmail.ru>
2026-08-11 14:11:38 +0300

    Code improvement

--- main.go
+++ main.go
@@ -3,37 +3,58 @@ package main
 import "fmt"
 
 func main() {
-	var a, b float64
-	var operator string
 
-	fmt.Println("Go-calculator")
+	for {
+		var a, b, result float64
+		var operator string
+		var choice int
 
-	fmt.Print("We enter the first number:")
-	fmt.Scan(&a)
+		fmt.Println("Go-calculator")
 
-	fmt.Print("We enter the operator: (+, -, *, /): ")
-	fmt.Scan(&operator)
+		getInput("The first number: ", &a)
 
-	fmt.Print("We enter the second number:")
-	fmt.Scan(&b)
+		getInput("Operator (+, -, *, /): ", &operator)
+		getInput("The second number: ", &b)
 
-	var result float64
+		switch operator {
+		case "+":
+			result = a + b
+		case "-":
+			result = a - b
+		case "*":
+			result = a * b
+		case "/":
+			if b == 0 {
+				fmt.Println("Error! Division by zero is impossible!")
+				continue
+			}
+			result = a / b
+		default:
+			fmt.Println("Error! Invalid mark!")
+			continue
+		}
+		fmt.Printf("%.2f %s %.2f = %.2f\n\n", a, operator, b, result)
+		fmt.Print("Do you want to continue? 1 - Yes, 2 - No: ")
+		fmt.Scan(&choice)
 
-	switch operator {
-	case "+":
-		result = a + b
-
-	case "-":
-		result = a - b
-
-	case "*":
-		result = a * b
-
-	case "/":
-		result = a / b
+		switch choice {
+		case 1:
+			continue
+		case 2:
+			fmt.Println("Bye!")
+			return
+		default:
+			fmt.Println("Error! Invalid number!")
+		}
 
 	}
+}
+func getInput(msg string, inp any) error {
+	for {
+		fmt.Print(msg)
 
-	fmt.Printf("%.2f %s %.2f = %.2f\n", a, operator, b, result)
+		_, err := fmt.Scan(inp)
 
+		return err
+	}
 }
